5fa5d669483f99bfa24b339181e04fd7b63de548
Frank Gu <frank.gu968@outlook.com>
2019-02-08 15:11:13 -0500

    code cleanup

--- redaction.py
+++ redaction.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -O
+# -*- coding: utf-8 -*-
+
+"""
+    File name: redaction.py
+    Author: Frank Gu
+    Date created: Feb 8, 2019
+    Date last modified: Feb 8, 2019
+    Python Version: 3.7
+"""
 
 import argparse
 from os import listdir
 from os.path import join, isfile
 import threading
-import datetime
+import time
 
-VERSION_STRING = "v0.1b"
+VERSION_STRING = "v1.0"
 
 terms = dict()      # Dictionary of terms
-input_folder = ""   # Globally visible input folder
-file_list = []
-list_lock = threading.Lock()
+file_list = []      # Shared list of files to process
+list_lock = threading.Lock()    # Lock for file_list
 
 
 def load_terms(f_input):
@@ -55,49 +63,66 @@ def process_file(f_input, input_folder, f_output):
         output_file.write(result_string)
 
 
-class fileProcessor(threading.Thread):
-    def __init__(self, input_folder, output_folder):
+class FileProcessor(threading.Thread):
+    def __init__(self, thread_id, input_folder, output_folder):
         threading.Thread.__init__(self)
         self.input_folder = input_folder
         self.output_folder = output_folder
+        self.thread_id = thread_id
 
     def run(self):
         global terms
         global file_list
-        while(file_list):
+
+        while file_list:
             list_lock.acquire()
             f_input = file_list.pop()
             list_lock.release()
-            print(f_input)
+            if __debug__:
+                # Only provide prints when in debug mode or performance will be poor!
+                print("Thread " + str(self.thread_id) + " processing: " + f_input)
             process_file(f_input, self.input_folder, self.output_folder)
 
-if __name__ == "__main__":
+
+def main():
+    global file_list
+
     parser = argparse.ArgumentParser(description="Batch document redactor " + VERSION_STRING)
     parser.add_argument('terms_list', default="terms.txt", help='Terms list path', type=str, nargs='?')
     parser.add_argument('input_folder', default="corpus/", help='Input text folder', type=str, nargs='?')
     parser.add_argument('output_folder', default="output/", help='Output text folder', type=str, nargs='?')
-    parser.add_argument('--n', help="Number of threads (default 4)", nargs='?', default=4, type=int)
+    parser.add_argument('--n', help="Number of threads (default 1)", nargs='?', default=1, type=int)
     args = parser.parse_args()
 
+    # List of files to process
     file_list = [f for f in listdir(args.input_folder) if isfile(join(args.input_folder, f))]
+    file_list.sort()
 
-    print("Batch document redeactor " + VERSION_STRING)
+    print("Batch document redactor " + VERSION_STRING)
     load_terms(args.terms_list)
     print("Terms list loaded, starting redaction...")
 
-    file_list.sort()
-    a = datetime.datetime.now()
-    threadPool = []
-    for i in range(args.n):
-        print("Starting thread")
-        newThread = fileProcessor(args.input_folder, args.output_folder)
-        threadPool.append(newThread)
-        newThread.start()
+    if __debug__:
+        a = time.time() # Timing facilities
 
-    for t in threadPool:
+    thread_pool = []
+    for i in range(args.n):
+        print("Starting thread " + str(i))
+        new_thread = FileProcessor(i, args.input_folder, args.output_folder)
+        thread_pool.append(new_thread)      # Add thread to pool
+        new_thread.start()                  # Start the thread
+
+    # Wait for all the threads to finish
+    for t in thread_pool:
         t.join()
-    b = datetime.datetime.now()
-    c = b - a
-    print(c.seconds)
+
+    if __debug__:
+        b = time.time()
+        c = b - a
+        print("Time elapsed: {:8.2f}s".format(c))
 
     print("Redaction complete!")
+
+
+if __name__ == "__main__":
+    main()
